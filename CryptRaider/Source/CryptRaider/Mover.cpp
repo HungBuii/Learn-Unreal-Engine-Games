f655caa03b31132016b932d20791849f434419e1
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-08-12 16:37:12 +0700

    update "CryptRaider" lesson 78->82

---
+++ CryptRaider/Source/CryptRaider/Mover.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* owner = GetOwner();
+	FString name = owner->GetActorNameOrLabel();
+	FVector ownerLocation = owner->GetActorLocation();
+	UE_LOG(LogTemp, Display, TEXT("Mover owner: %s, with location %s"), *name, *ownerLocation.ToCompactString());
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	
+}
+
